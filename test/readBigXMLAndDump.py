04643d1096a59d3074126606fbc78d6e46b6a298
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-09-16 22:22:46 +0000

    Make a single shell script to validate the geometry content and access is correct at the DDD level.

--- test/readBigXMLAndDump.py
+++ test/readBigXMLAndDump.py
@@ -1,24 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("DBGeometryTest")
-#process.load("DetectorDescription.OfflineDBLoader.test.cmsIdealGeometryForWrite_cfi")
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
+process = cms.Process("CompareGeometryTest")
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
         )
-process.source = cms.Source("EmptySource")
+
+process.source = cms.Source("EmptyIOVSource",
+                            lastValue = cms.uint64(1),
+                            timetype = cms.string('runnumber'),
+                            firstValue = cms.uint64(1),
+                            interval = cms.uint64(1)
+                            )
 
 process.myprint = cms.OutputModule("AsciiOutputModule")
 
-process.prod = cms.EDAnalyzer("TestGeometryAnalyzer",
-                                  ddRootNodeName = cms.string("cms:OCMS"),
-                                  dumpPosInfo = cms.untracked.bool(False),
-                                  dumpSpecs = cms.untracked.bool(False),
-                                  dumpGeoHistory = cms.untracked.bool(False)
+process.fred = cms.ESSource("XMLIdealGeometryESSource"
+                    ,geomXMLFiles = cms.vstring('GeometryReaders/XMLIdealGeometryESSource/test/fred.xml')
+                    ,rootNodeName = cms.string('cms:OCMS')
+                    )
+
+process.pABF = cms.EDAnalyzer("PerfectGeometryAnalyzer"
+                              ,ddRootNodeName = cms.string("cms:OCMS")
+                              ,dumpPosInfo = cms.untracked.bool(True)
+                              ,label = cms.untracked.string("fred")
+                              ,isMagField = cms.untracked.bool(False)
+                              ,dumpSpecs = cms.untracked.bool(True)
+                              ,dumpGeoHistory = cms.untracked.bool(True)
+                              ,outFileName = cms.untracked.string("BDB")
+                              ,numNodesToDump = cms.untracked.uint32(0)
                               )
-
-
 
 process.MessageLogger = cms.Service("MessageLogger",
     errors = cms.untracked.PSet(
@@ -50,8 +61,7 @@ process.MessageLogger = cms.Service("MessageLogger",
         'debug')
 )
 
-
 process.Timing = cms.Service("Timing")
 
-process.p1 = cms.Path(process.prod)
+process.p1 = cms.Path(process.pABF)
 process.e1 = cms.EndPath(process.myprint)
